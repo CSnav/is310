5d05a0c757cac1d4b4339c7c24ac7253ebdb12b1
CSnav <alande9@illinois.edu>
2022-04-28 03:18:25 -0500

    fix text_analysis

--- notebook_assignment/main.py
+++ notebook_assignment/main.py
@@ -2,26 +2,32 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
-def get_volumes(url):
+def get_volumes(url):   # get links of all volumes from directory
     soup = bs(requests.get(url).text, 'html.parser')
 
     data = []
     for link in soup.find_all('a'):
-        if link.text.startswith('humanist'):
+        if link.text.startswith('humanist'):  # valid links have humanist label
             data.append(link.text)
     return data
 
-def scrape_volume(url):
+def scrape_volume(url):  # scrape text
     r = requests.get(url)
     soup = bs(r.text, 'html.parser')
     return soup.get_text()
 
+def filter(doc):
+    tmp =  ''.join([i for i in doc if i.isalpha() or i.isspace()]).replace('\n', ' ').replace('\r', '').lower() # filter to only alphabets and space
+    while tmp.count('  ') > 0:
+        tmp = tmp.replace('  ', ' ') 
+    return tmp
+
 if __name__ == '__main__':
-    url = 'https://humanist.kdl.kcl.ac.uk/Archives/Converted_Text/'
+    url = 'https://humanist.kdl.kcl.ac.uk/Archives/Converted_Text/' # base url
     scripts = []
     vols = get_volumes(url)
     for vol in vols:
-        scripts.append({'url': url + vol, 'year': vol.split('.')[1], 'text': scrape_volume(url + vol)})
+        scripts.append({'url': url + vol, 'year': vol.split('.')[1], 'text': filter(scrape_volume(url + vol))})
     humanist_vols = pd.DataFrame(scripts)
     humanist_vols.to_csv('web_scraped_humanist_listserv.csv')
 
--- text_analysis_assignment/main.py
+++ text_analysis_assignment/main.py
@@ -1,24 +1,42 @@
 import nltk
 #nltk.download('punkt')
+#nltk.download('stopwords')
 #nltk.download('averaged_perceptron_tagger')
 import pandas as pd
+import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
 
-def top_1_year(df, year):
-    doc = str(df[df['year'] == year].text)
-    words = nltk.tokenize.word_tokenize(doc)
-    dists = nltk.FreqDist(w.lower() for w in words)
+stop_words = set(stopwords.words('english')) # set of stop words: the, as, etc
 
-    return dists.most_common(1)
+def top_1_year(df, year, verbose=False):  
+    doc = df[df['year'] == year].text.item()   # get text from df
+    words = nltk.tokenize.word_tokenize(doc)   # extract words
+    dists = nltk.FreqDist(w for w in words if w not in stop_words)
+    
+    if verbose:
+        return dists
+
+    return dists.most_common(1)[0]
 
 def top_1_all(df):
-    doc = '\n'.join(df.text.to_list())
+    doc = ' '.join(df.text.to_list())
     words = nltk.tokenize.word_tokenize(doc)
-    dists = nltk.FreqDist(w.lower() for w in words)
+    dists = nltk.FreqDist(w for w in words if w not in stop_words)
 
-    return dists.most_common(1)
+    return dists.most_common(1)[0]
 
-
+def plot(df, word):
+    x, y = [], []   # add year to x, freq to y
+    for i, row in df.iterrows():
+        dist = top_1_year(df, row.year, verbose=True)
+        if word not in dist:
+            raise ValueError('word not found')  # raise error if word not in corpus
+        x.append(row.year)
+        y.append(dist[word])
+    plt.plot(x, y)
+    plt.show()
 
 if __name__ == '__main__':
     df = pd.read_csv('../notebook_assignment/web_scraped_humanist_listserv.csv')
-    print(top_1_all(df))
+    print(top_1_year(df, '1987-1988'))
+    plot(df, 'I')
